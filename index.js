3ebe4c545a49ea19b63995df7f9c778b97e6df6f
Olivier Kathenis <olivier@olivierk.be>
2024-01-30 20:38:34 +0100

    correction phrase et documentation

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import { prompt } from "./prompt.js";
 
+/**
+ * Fonction validateInput
+ *
+ * Cette fonction valide l'entrée de l'utilisateur en fonction d'une condition spécifiée.
+ *
+ * @param {string} promptMessage - Le message à afficher à l'utilisateur pour lui demander une entrée.
+ * @param {function} condition - La condition que l'entrée de l'utilisateur doit respecter pour être valide.
+ * @param {string} errorMessage - Le message d'erreur à afficher si l'entrée de l'utilisateur n'est pas valide.
+ *
+ * @returns {string} - L'entrée de l'utilisateur si elle est valide.
+ */
 function validateInput(promptMessage, condition, errorMessage) {
   let input;
   do {
@@ -46,17 +57,27 @@ let phrase = validateInput(
 );
 
 let all = "";
-
+/**
+ * Fonction generatePassword
+ *
+ * Cette fonction génère un mot de passe en fonction des critères spécifiés par l'utilisateur.
+ *
+ * @param {number} numberCaracter - Le nombre de caractères que le mot de passe doit avoir.
+ * @param {string} specialCaracter - Indique si le mot de passe doit contenir des caractères spéciaux ("y" pour oui, "n" pour non).
+ * @param {string} number - Indique si le mot de passe doit contenir des chiffres ("y" pour oui, "n" pour non).
+ * @param {string} maj - Indique si le mot de passe doit contenir des majuscules ("y" pour oui, "n" pour non).
+ * @param {string} phrase - Une phrase dont les premières lettres de chaque mot seront utilisées pour générer le mot de passe.
+ *
+ * @returns {string} - Le mot de passe généré.
+ */
 const generatePassword = (
   numberCaracter,
   specialCaracter,
   number,
   maj,
-  phrase,
-  all
+  phrase
 ) => {
-  let password = "";
-  all += phrase
+  let lower = phrase
     .split(" ")
     .map((word) => word[0].toLowerCase())
     .join("");
@@ -64,18 +85,44 @@ const generatePassword = (
   const numbers = "0123456789";
   const special = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+  let conditions = [];
   if (specialCaracter === "y") {
-    all += special;
+    conditions.push(special);
   }
   if (number === "y") {
-    all += numbers;
+    conditions.push(numbers);
   }
   if (maj === "y") {
-    all += upper;
+    conditions.push(upper);
   }
-  for (let i = 0; i < numberCaracter; i++) {
-    password += all[Math.floor(Math.random() * all.length)];
+
+  let remainingCharacters = numberCaracter - lower.length;
+  let charactersPerCondition = Math.floor(
+    remainingCharacters / conditions.length
+  );
+
+  let password = lower; // Ajoute les premières lettres des mots de la phrase au mot de passe
+
+  conditions.forEach((condition) => {
+    for (let i = 0; i < charactersPerCondition; i++) {
+      password += condition[Math.floor(Math.random() * condition.length)];
+    }
+  });
+
+  // Si le mot de passe est plus court que le nombre de caractères demandé (à cause de l'arrondi), on ajoute des caractères
+  while (password.length < numberCaracter) {
+    let randomCondition =
+      conditions[Math.floor(Math.random() * conditions.length)];
+    password +=
+      randomCondition[Math.floor(Math.random() * randomCondition.length)];
   }
+
+  // Mélange le mot de passe
+  password = password
+    .split("")
+    .sort(() => 0.5 - Math.random())
+    .join("");
+
   return password;
 };
 
@@ -85,17 +132,6 @@ console.log(
     specialCaracter,
     number,
     maj,
-    phrase,
-    all
+    phrase
   )}`
 );
-
-// let all = phrase
-//   ? phrase
-//       .split(" ")
-//       .map((word) => word[0].toLowerCase())
-//       .join("")
-//   : "";
-
-// console.log(phrase);
-// console.log(all);
